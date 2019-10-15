07a7c4e21c40e9dce794438f3b378f3e8dd69009
Jonasvdham <Jonasvdham@gmail.com>
2019-10-15 21:57:00 +0100

    json parsing working on serverside, still buggy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
+require 'json'
+require 'net/http'
+require 'pp'
+
 class HomeController < ApplicationController
   def index
+    url = 'http://localhost:3000/continents.json'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    jsondata = JSON.parse(response)
+    countrydata = Array({"Country" => "Population"})
+    for i in 0..(jsondata.length-1) do
+      countrydata.push([jsondata[i]["Country"], jsondata[i]["Population"]])
+    end
+    puts countrydata
   end
 end
